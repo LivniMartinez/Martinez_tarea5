bbb869399567fd508541033341eb57c29b40aff8
Livni <llmc0383@gmail.com>
2023-07-14 12:02:54 -0600

    correccion

--- consulta.php
+++ consulta.php
@@ -2,7 +2,7 @@
 
 //Se utiliza el metodo GET, y se coloca un error al no utilizarlo
 if($_SERVER['REQUEST_METHOD']==='GET'){
-    $respuesta = "El metodo si es GET";
+    $respuesta = "El metodo es GET";
     echo $respuesta;
 }else {
     echo "ERROR el metodo no es GET";
